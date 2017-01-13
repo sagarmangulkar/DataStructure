15eee039ee8d4750598a67d018116b405e47fb91
Sagar <smangul1@binghamton.edu>
2017-01-13 05:05:56 -0500

    Implement an algorithm to determine if a string has all unique characters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,8 @@
 package com.company;
 
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
 //    static Student[] tempStudentObjArray;
@@ -30,6 +28,51 @@ public class Main {
         HashMap<Integer, Student> tempMap = buildHashMap(tempStudentObjArray);
         displayHashMap(tempMap);
 
+//--------------Implement an algorithm to determine if a string has all unique characters. What if you
+//--------------can not use additional data structures?
+        checkUniqueString();
+    }
+    
+//Checks unique string and identifies duplicate characters
+    private static void checkUniqueString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a string.");
+        String input = scanner.nextLine();
+        System.out.println("You have entered: " + input);
+        boolean isUnique = true;
+        StringBuffer repeatedCharSB = new StringBuffer();
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i+1; j < input.length(); j++) {
+                if(input.charAt(i) == input.charAt(j)){
+                    isUnique = false;
+                    repeatedCharSB.append(input.charAt(i));
+                }
+            }
+        }
+        for (int k = 0; k < repeatedCharSB.length(); k++) {
+            for (int l = k + 1; l < repeatedCharSB.length(); l++) {
+                if (repeatedCharSB.charAt(k) != '*') {
+                    if (repeatedCharSB.charAt(k) == repeatedCharSB.charAt(l)) {
+                        repeatedCharSB.deleteCharAt(l);
+                        repeatedCharSB.insert(l, '*');
+                    }
+                }
+            }
+        }
+        int sbLength = repeatedCharSB.length();
+        int starIndex;
+        for (int m = 0; m < sbLength; m++) {
+            starIndex =repeatedCharSB.indexOf("*");
+            if(starIndex >= 0) {
+                repeatedCharSB.deleteCharAt(starIndex);
+            }
+        }
+        if (!isUnique){
+            System.out.println("Entered string is not Unique, repeated characters: " + repeatedCharSB.toString());
+        }
+        else {
+            System.out.println("Entered string is Unique");
+        }
     }
 
     private static void displayStringBuffer(StringBuffer tempStudentStringBuffer) {
